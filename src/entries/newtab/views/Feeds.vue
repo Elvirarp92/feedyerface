9496733aa207b8beb7063473862f76fecb296ade
Elvira Ramírez <elvirarp92@gmail.com>
2024-09-29 20:29:22 +0200

    feat: html for new feed form

--- src/entries/newtab/views/Feeds.vue
+++ src/entries/newtab/views/Feeds.vue
@@ -1,8 +1,15 @@
 <template>
   <section>
     <h2>Feeeeeeeeeeeeds!</h2>
+    <form id="add-feed" action="index.html">
+      <h3>Introduce un nuevo feed a tu lista</h3>
+      <label for="title">Título</label>
+      <input required type="text" name="title" id="title" />
+      <label for="url">URL</label>
+      <input required type="url" name="url" id="url" />
+      <button type="submit">Añadir</button>
+    </form>
   </section>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
