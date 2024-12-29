d2cd451941a21bf54c3bc5f0956fd25dd27836f5
Elvira Ramírez <elvirarp92@gmail.com>
2024-12-30 00:26:17 +0100

    wip: fiddlin with polling feeds

--- src/entries/newtab/views/Articles.vue
+++ src/entries/newtab/views/Articles.vue
@@ -1,8 +1,41 @@
 <template>
   <section>
     <h2>Artículos</h2>
+    <iframe
+      ref="fetchIframe" 
+      src="https://www.eldiario.es/rss/"
+      frameborder="0"
+      @load="e => iframeIsLoaded(e)"
+    >
+    </iframe>
   </section>
 </template>
 
 <script setup>
+import { onBeforeUnmount, useTemplateRef, onMounted, reactive } from 'vue'
+
+const articles = reactive([])
+const frame = useTemplateRef('fetchIframe')
+
+function iframeIsLoaded(event) {
+  console.log('loadéd')
+  console.log(event.originalTarget)
+  console.log(event.originalTarget.contentWindow?.document)
+  console.log(frame.contentWindow?.document)
+}
+
+function afterLoading(){
+    alert("I am here");
+}
+
+onMounted(() => {
+  /* fetch('https://www.eldiario.es/rss/', {
+  }).then(res => console.log(res)) */
+
+
+})
+
+onBeforeUnmount(() => {
+})
+
 </script>
